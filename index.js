668f18f37f46f722770d133a7a034da133bb4170
C. Greyston Brady <c.greyston.brady@gmail.com>
2022-02-10 19:23:05 -0800

    First working version, three endpoints + home

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const res = require('express/lib/response')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/products', (req, res) => {
+  res.send('products visualization');
+
+  // TODO
+
+})
+
+app.get('/recipes', (req, res) => {
+  res.send('recipes visualization');
+
+  // TODO
+
+})
+
+app.get('/locations', (req, res) => {
+  res.send('locations visualization');
+
+  // TODO
+
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
