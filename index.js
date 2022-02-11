7c26de18e94582afe1208bdaa9c17ecc536dd49e
C. Greyston Brady <c.greyston.brady@gmail.com>
2022-02-10 22:42:15 -0800

    Added templating underbelly

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
-const express = require('express')
-const res = require('express/lib/response')
-const app = express()
-const port = 3000
+const express = require('express'),
+      cons = require('consolidate'),
+      res = require('express/lib/response'),
+      app = express(),
+      port = 3000;
+
+// assign the underscore engine to .html files
+app.engine('html', cons.underscore);
+
+// set .html as the default extension
+app.set('view engine', 'html');
+app.set('views', __dirname + '/views');
+
+var users = [];
+users.push({ name: 'tobi' });
+users.push({ name: 'loki' });
+users.push({ name: 'jane' });
+
 
 app.get('/', (req, res) => {
-  res.render('home');
+  res.render('users', {
+    title: 'Users',
+    users: users
+  });
 })
 
 app.get('/products', (req, res) => {
