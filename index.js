ff5e404262d38561a35b782498846c6f68d35edc
C. Greyston Brady <c.greyston.brady@gmail.com>
2022-02-17 22:52:04 -0800

    My recent machinations

--- index.js
+++ index.js
@@ -201,7 +201,7 @@ app.delete('/shopping', (req, res) => {
 })
 
 app.use((req, res, next) => {
-  res.status(404).send("404: Sorry can't find that!")
+  res.status(404).render("layout", {title: 'Shopping Lists'})
 })
 
 // Host it!
