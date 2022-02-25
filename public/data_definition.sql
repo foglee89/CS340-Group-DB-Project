467818b271f84251209f56851a51e1efb91cfdb8
fogle89 <foglee@oregonstate.edu>
2022-02-24 22:07:45 -0700

    table creation entered into data def file

--- public/data_definition.sql
+++ public/data_definition.sql
@@ -3,50 +3,91 @@
 --
 -- Products Table
 --
--- product_id (PK)
--- proudct_name
--- product_category
--- location_id (FK)
--- stored_quantity
--- unit
--- purchase_date
--- expiration_date
+
+CREATE TABLE Products (
+    product_id INT NOT NULL AUTO_INCREMENT,
+    product_name VARCHAR(50) NOT NULL,
+    product_category ENUM('Produce: Fruit', 'Produce: Vegetables', 
+        'Canned Goods', 'Sweets & Snacks', 'Meat, Poultry, & Seafood', 
+        'Eggs & Dairy', 'Grains & Bread', 'Condiments & Sauces', 'Dry Goods', 
+        'Alcohol', 'Drinks & Juices', 'Spices', 'Pre-Packaged Meals'),
+    location_id INT,
+    stored_quantity DECIMAL NOT NULL,
+    unit ENUM('oz - ounces', 'g-grams', 'kg - kilograms', 'ct - count', 
+        'bx - box', 'bg - bag', 'cn - can', 'btl - bottle', 'jr - jar', 
+        'lbs - pounds' 'Tbl - tablespoon', 'tsp - teaspoon'),
+    purchase_date DATE NOT NULL,
+    expiration_date DATE NOT NULL,
+    PRIMARY KEY (product_id)
+    FOREIGN KEY (location_id)
+    REFERENCES Locations(location_id)
+    ON DELETE CASCADE
+) ENGINE=InnoDB;
 
 --
 -- Recipes Table
 --
--- recipe_id (PK)
--- recipe_name
--- total_time
--- active_time
+
+CREATE TABLE Recipes (
+    recipe_id INT NOT NULL AUTO_INCREMENT,
+    recipe_name VARCHAR(50) NOT NULL,
+    total_time INT NOT NULL,
+    active_time INT NOT NULL,
+    PRIMARY KEY (recipe_id)
+) ENGINE=InnoDB;
 
 --
 -- RecipesProducts Table
 --
--- recipe_id (PK, FK)
--- product_id (PK, FK)
--- cooking_quantity
--- unit
+
+CREATE TABLE RecipesProducts (
+    recipe_id INT NOT NULL AUTO_INCREMENT,
+    product_id INT NOT NULL AUTO_INCREMENT,
+    cooking_quantity INT NOT NULL,
+    unit ENUM('oz - ounces', 'g-grams', 'kg - kilograms', 'ct - count', 
+        'bx - box', 'bg - bag', 'cn - can', 'btl - bottle', 'jr - jar', 
+        'lbs - pounds' 'Tbl - tablespoon', 'tsp - teaspoon'),
+    PRIMARY KEY (recipe_id, product id)
+    REFERENCES Recipes(recipe_id)
+    ON DELETE CASCADE
+    REFERENCES Products(product_id)
+) ENGINE=InnoDB;
 
 --
 -- Locations Table
 --
--- location_id (PK)
--- location_name
--- category
--- product_id (FK)
---
+
+CREATE TABLE Locations (
+    location_id INT NOT NULL AUTO_INCREMENT,
+    location_name VARCHAR(50) NOT NULL,
+    -- categories
+    -- product_id
+    PRIMARY KEY (location_id)
+) ENGINE=InnoDB;
 
 --
 -- ShoppingLists Table
 --
--- purchase_date (PK)
--- meal_plan_range
+
+CREATE TABLE ShoppingLists (
+    purchase_date DATE NOT NULL,
+    -- potentially different type depending on how SQL handles date ranges
+    meal_plan_range DATE NOT NULL,
+    PRIMARY KEY (purchase_date)
+) ENGINE=InnoDB;
 
 --
 -- ShoppingListProducts Table
 --
--- purchase_date (PK,FK)
--- product_id (PK, FK)
--- purchase_quantity
--- unit
+
+CREATE TABLE ShoppingListProducts (
+    purchase_date DATE NOT NULL, 
+    product_id INT NOT NULL,
+    purchase_quantity INT NOT NULL,
+    unit ENUM('oz - ounces', 'g-grams', 'kg - kilograms', 'ct - count', 
+        'bx - box', 'bg - bag', 'cn - can', 'btl - bottle', 'jr - jar', 
+        'lbs - pounds' 'Tbl - tablespoon', 'tsp - teaspoon'),
+    PRIMARY KEY (purchase_date, product_id)
+    REFERENCES ShoppingLists(purchase_date)
+    REFERENCES Products(product_id)
+) ENGINE=InnoDB;
