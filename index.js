009e1e16a446b078dc0d1c3ae68e20f7deea15db
fogle89 <foglee@oregonstate.edu>
2022-03-04 01:47:39 -0700

    commented out static mock data. need to debug and check routing for dynamic display/operations

--- index.js
+++ index.js
@@ -53,7 +53,7 @@ var siteMap = [
   // { name: "Meal Plans",     route: "/mealplans", }
 ];
 
-
+/*
 // MOCK DATA FROM STATIC DRAFT
 var persons = [
   { name: "Nils", age: 20 },
@@ -79,7 +79,7 @@ var recipes = [
 var shopping = [
   {purchase_date: "2022-02-11", meal_plan_range: "2022-02-11 to 2022-02-12"},
 ];
-
+*/
 
 // Register Partials with Handlebars
 Handlebars.registerPartial(
